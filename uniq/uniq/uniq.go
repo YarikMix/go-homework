8c98ccae8168b75ce5f38519503330b8be3ec4f8
YarikMix <43493788+YarikMix@users.noreply.github.com>
2024-03-01 01:48:30 +0300

    Поправил uniq

--- uniq/uniq/uniq.go
+++ uniq/uniq/uniq.go
@@ -74,31 +74,40 @@ func Compare(str1 string, str2 string, args Args) bool {
 	return getLine(str1, args) == getLine(str2, args)
 }
 
-func FilterLines(lines []string, args Args) (arr []string) {
-	var tmp string = lines[0]
+func Solve(lines []string, args Args) (arr []string) {
 
-	arr = append(arr, lines[0])
+	var tmp string
+	var c = 0
+	var f = true
 
-	for _, line := range lines[1:] {
+	for _, line := range lines {
 
-		if !Compare(tmp, line, args) {
-			arr = append(arr, line)
-		}
+		if args.D {
 
-		tmp = line
-	}
+			if f && Compare(tmp, line, args) {
+				arr = append(arr, line)
+				f = false
+			} else {
+				f = true
+			}
 
-	return
-}
+		} else if args.U {
 
-func CountOccurrencesNumber(lines []string, args Args) (arr []string) {
-	var tmp string = lines[0]
-	var c = 1
+			if count(line, lines) == 1 {
+				arr = append(arr, line)
+			}
 
-	for _, line := range lines[1:] {
-		if !Compare(tmp, line, args) {
-			s := fmt.Sprintf("%d %s", c, tmp)
-			arr = append(arr, s)
+		} else if !Compare(tmp, line, args) {
+
+			if args.C {
+				if c > 0 {
+					s := fmt.Sprintf("%d %s", c, tmp)
+					arr = append(arr, s)
+				}
+			} else {
+				arr = append(arr, line)
+			}
+
 			c = 0
 		}
 
@@ -106,52 +115,10 @@ func CountOccurrencesNumber(lines []string, args Args) (arr []string) {
 		c += 1
 	}
 
-	s := fmt.Sprintf("%d %s", c, tmp)
-	arr = append(arr, s)
-
-	return
-}
-
-func GetDuplicateLines(lines []string, args Args) (arr []string) {
-	var prevLine string = lines[0]
-	var f = true
-
-	for _, line := range lines[1:] {
-		if f && Compare(prevLine, line, args) {
-			arr = append(arr, line)
-			f = false
-		} else {
-			f = true
-		}
-
-		prevLine = line
-	}
-
-	return
-}
-
-func GetUniqueLines(lines []string, args Args) (arr []string) {
-	for _, line := range lines {
-		if count(line, lines) == 1 {
-			arr = append(arr, line)
-		}
-	}
-
-	return
-}
-
-func Solve(lines []string, args Args) []string {
-
-	var arr = make([]string, 0)
 	if args.C {
-		arr = CountOccurrencesNumber(lines, args)
-	} else if args.D {
-		arr = GetDuplicateLines(lines, args)
-	} else if args.U {
-		arr = GetUniqueLines(lines, args)
-	} else {
-		arr = FilterLines(lines, args)
+		s := fmt.Sprintf("%d %s", c, tmp)
+		arr = append(arr, s)
 	}
 
-	return arr
+	return
 }
